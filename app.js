e1ea2d5907aebe765c18bec6a70511fad67bc7a6
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-06 15:58:05 +1200

    feat: Added JSON conversion, URL encoding and cookie parsing to API

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(json());
+app.use(urlencoded({ "extended": false }));
+app.use(cookieParser());
 
 export default app;
