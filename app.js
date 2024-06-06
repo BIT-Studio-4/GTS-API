794e20d624217df3227a0c69ba43423d2fc00633
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-06 16:10:05 +1200

    feat: Added /api routes to API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import apiRoutes from "./routes/api/index.js";
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
     res.send("Welcome to the GTS homepage!");
 });
+
+app.use("/api", apiRoutes);
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000.");
