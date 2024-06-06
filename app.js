abb8d1dbcbb31072a6b7db7c3954050f87baeef6
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-06 16:14:12 +1200

    fix: Added missing security features

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import apiRoutes from "./routes/api/index.js";
 
+const setXContentTypeOptions = (req, res, next) => {
+    res.set("x-content-type-options", "nosniff");
+    next();
+};
+
+const setXFrameOptions = (req, res, next) => {
+    res.set("x-frame-options", "deny");
+    next();
+};
+const setContentSecurityPolicy = (req, res, next) => {
+    res.set("content-security-policy", "default-src 'none'");
+    next();
+};
+
 const app = express();
 
 app.use(cors());
+app.use(setXContentTypeOptions);
+app.use(setXFrameOptions);
+app.use(setContentSecurityPolicy);
 app.use(urlencoded({ "extended": false }));
 app.use(json());
 app.use(cookieParser());
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the GTS homepage!");
