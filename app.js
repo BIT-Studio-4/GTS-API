4db19b2c6c4e0bdd7bb40c68309496bf5b9120eb
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-06 16:01:27 +1200

    feat: Added external connection listener on port 3000

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.get("/", (req, res) => {
     res.send("Welcome to the GTS homepage!");
 });
 
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000.");
+});
+
 export default app;
