6f633f1f10af3bb195b9dedbd3cadeaea10f11a7
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-11 16:03:35 +1200

    docs: Add descriptions to the main API app.js file.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const setXFrameOptions = (req, res, next) => {
     res.set("x-frame-options", "deny");
     next();
 };
+
 const setContentSecurityPolicy = (req, res, next) => {
     res.set("content-security-policy", "default-src 'none'");
     next();
@@ -19,14 +20,13 @@ const setContentSecurityPolicy = (req, res, next) => {
 
 const app = express();
 
-app.use(cors());
-app.use(setXContentTypeOptions);
-app.use(setXFrameOptions);
-app.use(setContentSecurityPolicy);
-app.use(urlencoded({ "extended": false }));
-app.use(json());
-app.use(cookieParser());
-
+app.use(cors()); // Allows non-same URLs to access the API.
+app.use(setXContentTypeOptions); // Forces the browser receiving a response to accept the provided type instead of trying to guess the type. E.g., 'application/json'.
+app.use(setXFrameOptions); // Prevents browsers from displaying the API in a frame of another site.
+app.use(setContentSecurityPolicy); // Prevents the browser from loading resources obtained by untrusted sources.
+app.use(urlencoded({ "extended": false })); // Allows info sent in the URL to be read by the API.
+app.use(json()); // Allows the API to parse requests as JSON.
+app.use(cookieParser()); // Allows cookies to be controlled by the API. Good for Authorization.
 
 app.get("/", (req, res) => {
     res.send("Welcome to the GTS homepage!");
@@ -34,6 +34,7 @@ app.get("/", (req, res) => {
 
 app.use("/api", apiRoutes);
 
+// Starts the API on port 3000. E.g., http://localhost:3000.
 app.listen(3000, () => {
     console.log("Server is listening on port 3000.");
 });
