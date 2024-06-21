da7e9ece827509c5557c713573595dda85e15b03
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-21 20:25:37 +1200

    feat: Added check for existing user clash

--- controllers/user.js
+++ controllers/user.js
@@ -9,6 +9,12 @@ const createUser = async (req, res) => {
     if (!contentType || contentType !== "application/json")
       return res.status(400).json({ "msg": "Invalid Content-Type. Expected 'application/json'." });
 
+    let user = await prisma.user.findUnique({
+      "where": { "name": String(req.body.name) },
+    });
+
+    // Check that a user with the same unique info doesn't already exist.
+    if (user) return res.status(403).json({ "msg": "User already exists." });
 
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
