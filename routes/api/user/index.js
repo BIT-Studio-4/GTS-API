fed71df11f5d5320a3a9e7f56b142754bc987f89
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-21 20:31:19 +1200

    fix: Fixed incorrect user controller import in /user routes

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser } from "../../../controllers/user";
+import { createUser } from "../../../controllers/user.js";
 
 const router = express.Router();
 
