464d48a5e426b559e15dddd67c58c7c80296d5b1
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-21 20:29:00 +1200

    feat: Connected create user request handler to /user routes

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { createUser } from "../../../controllers/user";
 
 const router = express.Router();
+
+router.post("/", createUser);
 
 export default router;
