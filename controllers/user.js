86f6c4536de00bb2ddf6ffa4c4650c07c66f94d2
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-21 20:25:09 +1200

    feat: Added content-type header check

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,11 @@ const prisma = new PrismaClient();
 
 const createUser = async (req, res) => {
   try {
+    // Ensure that the request info is formatted correctly.
+    const contentType = req.headers["content-type"];
+    if (!contentType || contentType !== "application/json")
+      return res.status(400).json({ "msg": "Invalid Content-Type. Expected 'application/json'." });
+
 
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
