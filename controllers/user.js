49f48ea5c3151098f4e7ae0ada676ae3df37209d
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:00:27 +1200

    feat: Added check for correct request content formatting

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 const updateUser = async (req, res) => {
   try {
+    const contentType = req.headers["content-type"];
+    if (!contentType || contentType !== "application/json")
+      return res.status(400).json({ "msg": "Invalid Content-Type. Expected 'application/json'." });
     
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
