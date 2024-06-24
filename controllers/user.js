f8c04b2fa85d2ad026df9d45c8e0290a53b682dc
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 23:27:43 +1200

    docs: Added descriptions to the user controller and routes

--- controllers/user.js
+++ controllers/user.js
@@ -4,10 +4,12 @@ const prisma = new PrismaClient();
 
 const updateUser = async (req, res) => {
   try {
+    // Check if the request is using the correct format for the API to parse.
     const contentType = req.headers["content-type"];
     if (!contentType || contentType !== "application/json")
       return res.status(400).json({ "msg": "Invalid Content-Type. Expected 'application/json'." });
     
+    // Get the requested user by the id/name.
     const queryUser = await prisma.user.findMany({
       "where": {
         "OR": [
@@ -18,15 +20,18 @@ const updateUser = async (req, res) => {
     });
     let user = queryUser[0];
 
+    // Check if the user exists, if not return a 404 not found response.
     if (!user) return res.status(404).json({ "msg": `User '${req.params.id}' not found.` });
 
+    // Update the user with the provided information.
     user = await prisma.user.update({
       "where": { "name": String(user.name) },
       "data": { ...req.body },
     });
 
-    delete user.password;
+    delete user.password; // Delete confidential info (password). More efficient than making another GET request.
 
+    // If the update request was successful, return the new user with a 200 success message.
     return res.status(200).json({
       "msg": `User '${user.name} successfully updated!`,
       "data": user,
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import { updateUser } from "../../../controllers/user.js";
 
 const router = express.Router();
 
+// Run updateUser whenever a PUT request with the 'id' parameter is made to /users
 router.put("/:id", updateUser);
 
 export default router;
