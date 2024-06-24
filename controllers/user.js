71fa1c6ad30e0f2c6afec9b48b3c197d70c50827
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:04:42 +1200

    feat: Added invalid user check

--- controllers/user.js
+++ controllers/user.js
@@ -18,6 +18,8 @@ const updateUser = async (req, res) => {
     });
     let user = queryUser[0];
 
+    if (!user) return res.status(404).json({ "msg": `User '${req.params.id}' not found.` });
+
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
   }
