c276d132783b2f685dfe73bb1464b9e5f410cb29
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 23:30:41 +1200

    docs: Added descriptions to the user controller and routes

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const prisma = new PrismaClient();
 
 const deleteUser = async (req, res) => {
   try {
+    // Get the requested user.
     const queryUser = await prisma.user.findMany({
       "where": {
         "OR": [
@@ -14,12 +15,15 @@ const deleteUser = async (req, res) => {
     });
     const user = queryUser[0];
 
+    // Check if the requested user exists, if not return a 404 not found response.
     if (!user) return res.status(404).json({ "msg": `User '${req.params.id}' not found.` });
 
+    // Delete the user.
     await prisma.user.delete({
       "where": { "name": String(user.name) },
     });
 
+    // If the delete request was successful return a 200 success message.
     return res.status(200).json({
       "msg": `User '${user.name}' successfully deleted!`,
     });
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import { deleteUser } from "../../../controllers/user.js";
 
 const router = express.Router();
 
+// Run deleteUser whenever a DELETE request is made to /users with the 'id' parameter.
 router.delete("/:id", deleteUser);
 
 export default router;
