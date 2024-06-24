d2894e7b51db7c8c6ee25c1521dd75be6cf2c7fd
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 15:45:42 +1200

    fix: Removed dangerous info returning from getUsers

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,12 @@ const prisma = new PrismaClient();
 
 const getUsers = async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      "select": {
+        "name": true,
+        "money": true,
+      },
+    });
 
     if (users.length === 0) return res.status(404).json({ "msg": "No users found." });
 
