70dce71bae556bccfa57159f3b58b1e8c21a2f75
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 15:35:02 +1200

    feat: Added user querying with missing users check

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,11 @@ const prisma = new PrismaClient();
 
 const getUsers = async (req, res) => {
   try {
-    
+    const users = await prisma.user.findMany();
+
+    if (users.length === 0) return res.status(404).json({ "msg": "No users found." });
+
+    return res.status(200).json({ "data": users });
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
   }
