0f2d342a9e7bc2f47954935b48b7f669b56d8f40
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:08:02 +1200

    feat: Return user upon successful update

--- controllers/user.js
+++ controllers/user.js
@@ -25,6 +25,10 @@ const updateUser = async (req, res) => {
       "data": { ...req.body },
     });
 
+    return res.status(200).json({
+      "msg": `User '${user.name} successfully updated!`,
+      "data": user
+    });
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
   }
