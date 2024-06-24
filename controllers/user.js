347ee82e7a4d75063f7da936513de03f8e6ae723
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 23:12:56 +1200

    docs: Added descriptions to the user controller and routes

--- controllers/user.js
+++ controllers/user.js
@@ -5,14 +5,17 @@ const prisma = new PrismaClient();
 const getUsers = async (req, res) => {
   try {
     const users = await prisma.user.findMany({
+      // Request only the name and money fields when querying the database. Makes sure it won't provide anything confidential or dangerous.
       "select": {
         "name": true,
         "money": true,
       },
     });
 
+    // If the database couldn't find any users, return a 404 not found response.
     if (users.length === 0) return res.status(404).json({ "msg": "No users found." });
 
+    // If the query was successful, return the users with a 200 success response.
     return res.status(200).json({ "data": users });
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import { getUsers } from "../../../controllers/user.js";
 
 const router = express.Router();
 
+// Run getUsers when a GET request is made to /users
 router.get("/", getUsers);
 
 export default router;
