40bc8036630575e370a6c7e7cbb85e3ec7ec6861
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 22:51:57 +1200

    docs: Added description to /users route

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import { createUser } from "../../../controllers/user.js";
 
 const router = express.Router();
 
+// Run createUser on any POST request made to the /users route.
 router.post("/", createUser);
 
 export default router;
