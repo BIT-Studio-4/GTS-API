976cc609ffd26d9825c8ce30eadc03ed1238aac6
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 20:37:37 +1200

    feat: Added user search on id and name

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,16 @@ const prisma = new PrismaClient();
 
 const getUser = async (req, res) => {
   try {
-    
+    const queryUser = await prisma.user.findMany({
+      "where": {
+        "OR": [
+          { "id": { "equals": String(req.params.id) }},
+          { "name": { "equals": String(req.params.id) }},
+        ],
+      },
+    });
+    const user = queryUser[0];
+
   } catch (error) {
     return res.status(500).json({ "msg": error.message });
   }
