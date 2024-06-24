f144cca0ba1ce95260436298b5ac352fb984ae0b
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 23:23:43 +1200

    docs: Added descriptions to the user controller and routes

--- controllers/user.js
+++ controllers/user.js
@@ -5,22 +5,26 @@ const prisma = new PrismaClient();
 const getUser = async (req, res) => {
   try {
     const queryUser = await prisma.user.findMany({
+      // Search for a user with either an id or name that matches the id parameter.
       "where": {
         "OR": [
           { "id": { "equals": String(req.params.id) }},
           { "name": { "equals": String(req.params.id) }},
         ],
       },
+      // Only requests information that isn't confidential or dangerous.
       "select": {
         "id": true,
         "money": true,
         "name": true,
       },
     });
-    const user = queryUser[0];
+    const user = queryUser[0]; // Since findMany returns a list, take the first result. This should always work since the id and name fields require unique entries.
 
+    // If the user doesn't exist, return a 404 not found response.
     if (!user) return res.status(404).json({ "msg": `User '${req.params.id}' not found.` });
 
+    // If the request was successful, return the user and a 200 success status.
     return res.status(200).json({
       "data": user,
     });
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -3,6 +3,7 @@ import { getUser } from "../../../controllers/user.js";
 
 const router = express.Router();
 
+// Run getUser whenever a GET request with the 'id' parameter is used.
 router.get("/:id", getUser);
 
 export default router;
