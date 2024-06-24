550133bdfd8aef75af55bbd00e017f35c6cb0c62
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:28:32 +1200

    feat: Connected deleteUser to /users route

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { deleteUser } from "../../../controllers/user.js";
 
 const router = express.Router();
+
+router.delete("/:id", deleteUser);
 
 export default router;
