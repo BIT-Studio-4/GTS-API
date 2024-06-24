0f867b0ed60a62797fab676a89acf93c23c1d6ae
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:23:35 +1200

    feat: Added user search query to deleteUser

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,20 @@ const prisma = new PrismaClient();
 
 const deleteUser = async (req, res) => {
   try {
-    
+    const queryUser = await prisma.user.findMany({
+      "where": {
+        "OR": [
+          { "id": { "equals": String(req.params.id) }},
+          { "name": { "equals": String(req.params.id) }},
+        ],
+      },
+      "select": {
+        "id": true,
+        "money": true,
+        "name": true,
+      },
+    });
+    const user = queryUser[0];
   } catch (error) {
     return res.status(500).json({
       "msg": error.message,
