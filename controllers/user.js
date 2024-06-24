a251e3de99507fb8a80099d796904e60f6ac2157
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 20:58:27 +1200

    feat: Added basic error handling to updateUser

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const updateUser = async (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    return res.status(500).json({ "msg": error.message });
+  }
 };
 
 export {
