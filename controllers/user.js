f031922eb4f2a5f853cdc4eaf24d2dded52bdcc4
wilsjw <wilsjw2@student.op.ac.nz>
2024-06-24 21:22:22 +1200

    feat: Added basic error handling to deleteUser

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const deleteUser = async (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    return res.status(500).json({
+      "msg": error.message,
+    });
+  }
 };
 
 export {
