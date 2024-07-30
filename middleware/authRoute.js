3be318e3bcd2850729cb02c5b3df92f5da118b87
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-30 21:01:38 +1200

    feat: Save the token's payload to the request if all checks pass

--- middleware/authRoute.js
+++ middleware/authRoute.js
@@ -6,6 +6,14 @@ const authRoute = (req, res, next) => {
     const authHeader = req.headers["authorization"];
     if (!authHeader || !authHeader.startsWith("Bearer"))
       return res.status(403).json({ "msg": "No token provided." });
+
+    // Check if the token is valid in the API
+    const token = authHeader.split(" ")[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = payload; // Save the received user payload from the token to the request
+
+    return next();
   } catch (error) {
     return res.status(403).json({ "msg": "Not authorized to access this route." });
   }
