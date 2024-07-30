1b2f0703511880d272f12d3dce95cd16816f08fd
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-30 20:23:41 +1200

    feat: Set up basic error handling in register

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const register = async (req, res) => {
-
+  try {
+    
+  } catch (error) {
+    return res.status(500).json({ "msg": error.message });
+  }
 };
 
 const login = async (req, res) => {
