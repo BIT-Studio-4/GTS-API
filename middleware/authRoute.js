a384ce155a0dc814fb590859da7e49e33210e41f
wilsjw <wilsjw2@student.op.ac.nz>
2024-07-30 20:58:15 +1200

    feat: Added valid token in authorization header check

--- middleware/authRoute.js
+++ middleware/authRoute.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 
 const authRoute = (req, res, next) => {
   try {
-    
+    // Check if a valid token has been provided
+    const authHeader = req.headers["authorization"];
+    if (!authHeader || !authHeader.startsWith("Bearer"))
+      return res.status(403).json({ "msg": "No token provided." });
   } catch (error) {
     return res.status(403).json({ "msg": "Not authorized to access this route." });
   }
