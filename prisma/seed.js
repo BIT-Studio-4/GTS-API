22fa33ca59d85d86c4fde1f6777b20571c7abb6e
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-10 14:28:20 +1200

    feat: Changed seed script to use upsert instead of create

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,15 +1,39 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const main = async () => {
-  try {
-    await prisma.user.create({
-      data: {
+const users = [
+    {
         name: 'dave',
         password: 'DavePassword!',
         money: 20
-      },
-    });
+    },
+    {
+        name: 'jeffrey',
+        password: 'BetterPassword24!',
+        money: 15
+    },
+    {
+        name: 'amanda',
+        password: 'SuperDuperPassword98_',
+        money: 300
+    },
+];
+
+const main = async () => {
+  try {
+    for (let user in users) {
+        await prisma.user.upsert({
+            where: {
+                name: user.name,
+            },
+            update: {
+                ...user
+            },
+            create: {
+                ...user
+            },
+        });
+    }
   } catch (err) {
     console.error(err);
   } finally {
