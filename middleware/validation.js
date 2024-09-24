2e18a67f2106ac7caebaafdf96101d87c9f7cbc8
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-24 13:59:26 +1200

    feat: Created validateAuth for auth requests

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,4 +1,15 @@
-import { userSchema } from "./validationSchemas";
+import { authSchema, userSchema } from "./validationSchemas";
+
+const validateAuth = (req, res, next) => {
+  const { error } = authSchema.validate(req.body);
+
+  if (error)
+    return res.status(400).json({
+      "msg": error.details[0].message,
+    });
+  
+  next();
+};
 
 const validateMutateUser = (req, res, next) => {
   const { error } = userSchema.validate(req.body);
@@ -9,4 +20,9 @@ const validateMutateUser = (req, res, next) => {
     });
 
   next();
+};
+
+export {
+  validateAuth,
+  validateMutateUser,
 };
