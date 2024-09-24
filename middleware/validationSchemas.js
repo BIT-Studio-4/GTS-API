a15940d2db9d899ad87940303118bc7a1174fba4
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-24 13:57:06 +1200

    feat: Created authSchema for register/login requests

--- middleware/validationSchemas.js
+++ middleware/validationSchemas.js
@@ -5,6 +5,11 @@
 
 import Joi from "joi";
 
+export const authSchema = Joi.object({
+  name: Joi.string().min(1).max(25).regex(/[A-Za-z0-9_-!@#$%^&*()?]/).required(),
+  password: Joi.string().min(8).max(128).required(),
+});
+
 export const userSchema = Joi.object({
   name: Joi.string().min(1).max(25).regex(/[A-Za-z0-9_-!@#$%^&*()?]/).required(),
   password: Joi.string().min(8).max(128).required(),
