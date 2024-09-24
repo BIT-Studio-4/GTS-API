05ff5f23bfb16cb57afbddf05f4a78dcacfa928e
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-24 15:01:18 +1200

    feat: Added error messages when validations fail

--- middleware/validationSchemas.js
+++ middleware/validationSchemas.js
@@ -5,13 +5,45 @@
 
 import Joi from "joi";
 
+const nameRegex = /[A-Za-z]+[0-9]+!@#\$%\^&\*\(\)_-\?/;
+
 export const authSchema = Joi.object({
-  name: Joi.string().min(1).max(25).regex(/[A-Za-z0-9_-!@#$%^&*()?]/).required(),
-  password: Joi.string().min(8).max(128).required(),
+  name: Joi.string().min(1).max(25).regex(nameRegex).required().messages({
+    "string.base": "Name should be a string.",
+    "string.pattern.base": "Name can only consist of Alphanumeric and '!@#$%^&*()_-?' characters.",
+    "string.empty": "Name cannot be empty.",
+    "string.min": "Name should have a minimum length of {#limit}.",
+    "string.max": "Name should have a maximum length of {#limit}.",
+    "any.required": "Name is required.",
+  }),
+  password: Joi.string().min(8).max(128).required().messages({
+    "string.base": "Password should be a string.",
+    "string.empty": "Password cannot be empty.",
+    "string.min": "Password should have a minimum length of {#limit}.",
+    "string.max": "Password should have a maximum length of {#limit}.",
+    "any.required": "Password is required.",
+  }),
 });
 
 export const userSchema = Joi.object({
-  name: Joi.string().min(1).max(25).regex(/[A-Za-z0-9_-!@#$%^&*()?]/).required(),
-  password: Joi.string().min(8).max(128).required(),
-  money: Joi.number().required(),
+  name: Joi.string().min(1).max(25).regex(nameRegex).required().messages({
+    "string.base": "Name should be a string.",
+    "string.pattern.base": "Name can only consist of Alphanumeric and '!@#$%^&*()_-?' characters.",
+    "string.empty": "Name cannot be empty.",
+    "string.min": "Name should have a minimum length of {#limit}.",
+    "string.max": "Name should have a maximum length of {#limit}.",
+    "any.required": "Name is required.",
+  }),
+  password: Joi.string().min(8).max(128).required().messages({
+    "string.base": "Password should be a string.",
+    "string.empty": "Password cannot be empty.",
+    "string.min": "Password should have a minimum length of {#limit}.",
+    "string.max": "Password should have a maximum length of {#limit}.",
+    "any.required": "Password is required.",
+  }),
+  money: Joi.number().required().messages({
+    "number.base": "Money must be a number.",
+    "number.unsafe": "Money is outside of usable range of numbers.",
+    "any.required": "Money is required.",
+  }),
 });
