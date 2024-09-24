d41b0f50ec3eb3678e27dc1958453c5df1a9f499
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-24 13:44:28 +1200

    feat: Created userSchema using User model fields

--- middleware/validationSchemas.js
+++ middleware/validationSchemas.js
@@ -6,5 +6,7 @@
 import Joi from "joi";
 
 export const userSchema = Joi.object({
-  
+  name: Joi.string().min(1).max(25).regex(/[A-Za-z0-9_-!@#$%^&*()?]/).required(),
+  password: Joi.string().min(8).max(128).required(),
+  money: Joi.number().required(),
 });
