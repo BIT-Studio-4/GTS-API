716b2add8759300cbd6b174b2eac0c5cca5657ba
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-24 14:05:22 +1200

    feat: Added validation to auth and user routes

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -6,18 +6,19 @@ import {
   updateUser,
   deleteUser,
 } from "../../../controllers/user.js";
+import { validateMutateUser } from "../../../middleware/validation.js";
 
 const router = express.Router();
 
 // Run createUser on any POST request made to the /users route.
-router.post("/", createUser);
+router.post("/", validateMutateUser, createUser);
 // Run getUsers when a GET request is made to /users
 router.get("/", getUsers);
 // Run getUser whenever a GET request with the 'id' parameter is used.
 router.get("/:id", getUser);
 // Run updateUser whenever a PUT request with the 'id' parameter is made to /users
-router.put("/:id", updateUser);
+router.put("/:id", validateMutateUser, updateUser);
 // Run deleteUser whenever a DELETE request is made to /users with the 'id' parameter.
-router.delete("/:id", deleteUser);
+router.delete("/:id", validateMutateUser, deleteUser);
 
 export default router;
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../../controllers/auth.js";
+import { validateAuth } from "../../middleware/validation.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateAuth, register);
+router.post("/login", validateAuth, login);
 
 export default router;
