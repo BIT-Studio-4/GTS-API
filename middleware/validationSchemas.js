5fc280411b9c1e62d0452b75bf64ad4511bc59ee
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-15 22:50:38 +1300

    feat: Created item validation schemas

--- middleware/validationSchemas.js
+++ middleware/validationSchemas.js
@@ -7,24 +7,6 @@ import Joi from "joi";
 
 const nameRegex = /^[A-Za-z0-9_!@#\$%\^&\*\(\)\-\?]+$/;
 
-export const authSchema = Joi.object({
-  name: Joi.string().min(1).max(25).regex(nameRegex).required().messages({
-    "string.base": "Name should be a string.",
-    "string.pattern.base": "Name can only consist of Alphanumeric and '!@#$%^&*()_-?' characters.",
-    "string.empty": "Name cannot be empty.",
-    "string.min": "Name should have a minimum length of {#limit}.",
-    "string.max": "Name should have a maximum length of {#limit}.",
-    "any.required": "Name is required.",
-  }),
-  password: Joi.string().min(8).max(128).required().messages({
-    "string.base": "Password should be a string.",
-    "string.empty": "Password cannot be empty.",
-    "string.min": "Password should have a minimum length of {#limit}.",
-    "string.max": "Password should have a maximum length of {#limit}.",
-    "any.required": "Password is required.",
-  }),
-});
-
 export const createUserSchema = Joi.object({
   name: Joi.string().min(1).max(25).regex(nameRegex).required().messages({
     "string.base": "Name should be a string.",
@@ -40,11 +22,6 @@ export const createUserSchema = Joi.object({
     "string.min": "Password should have a minimum length of {#limit}.",
     "string.max": "Password should have a maximum length of {#limit}.",
     "any.required": "Password is required.",
-  }),
-  money: Joi.number().required().messages({
-    "number.base": "Money must be a number.",
-    "number.unsafe": "Money is outside of usable range of numbers.",
-    "any.required": "Money is required.",
   }),
 });
 
@@ -63,9 +40,51 @@ export const updateUserSchema = Joi.object({
     "string.min": "Password should have a minimum length of {#limit}.",
     "string.max": "Password should have a maximum length of {#limit}.",
   }),
-  money: Joi.number().required().messages({
-    "number.base": "Money must be a number.",
-    "number.unsafe": "Money is outside of usable range of numbers.",
-    "any.required": "Money is required.",
+});
+
+const itemRegex = /^[A-Za-z0-9_\(\)]+$/;
+const itemTypeRegex = /^[A-Z]+$/;
+
+export const createItemSchema = Joi.object({
+  name: Joi.string().min(1).max(25).regex(itemRegex).required().messages({
+    "string.base": "Name should be a string.",
+    "string.pattern.base": "Name can only consist of Alphanumeric and '!@#$%^&*()_-?' characters.",
+    "string.empty": "Name cannot be empty.",
+    "string.min": "Name should have a minimum length of {#limit}.",
+    "string.max": "Name should have a maximum length of {#limit}.",
+    "any.required": "Name is required.",
+  }),
+  item_type: Joi.string().regex(itemTypeRegex).required().messages({
+    "string.base": "Item type should be a string.",
+    "string.pattern.base": "Item type can only consist of captial letters.",
+    "string.empty": "Item type cannot be empty.",
+    "any.required": "Item type is required.",
+  }),
+  cost: Joi.number().required().messages({
+    "number.base": "Cost must be a number.",
+    "number.unsafe": "Cost is outside of usable range of numbers.",
+    "any.required": "Cost is required.",
+  }),
+});
+
+export const updateItemSchema = Joi.object({
+  name: Joi.string().min(1).max(25).regex(itemRegex).messages({
+    "string.base": "Name should be a string.",
+    "string.pattern.base": "Name can only consist of Alphanumeric and '!@#$%^&*()_-?' characters.",
+    "string.empty": "Name cannot be empty.",
+    "string.min": "Name should have a minimum length of {#limit}.",
+    "string.max": "Name should have a maximum length of {#limit}.",
+    "any.required": "Name is required.",
+  }),
+  item_type: Joi.string().regex(itemTypeRegex).messages({
+    "string.base": "Item type should be a string.",
+    "string.pattern.base": "Item type can only consist of captial letters.",
+    "string.empty": "Item type cannot be empty.",
+    "any.required": "Item type is required.",
+  }),
+  cost: Joi.number().messages({
+    "number.base": "Cost must be a number.",
+    "number.unsafe": "Cost is outside of usable range of numbers.",
+    "any.required": "Cost is required.",
   }),
 });
