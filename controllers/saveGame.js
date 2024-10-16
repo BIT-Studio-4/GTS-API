a63e677deb1b2e5b26318b19c6b8e6694fd52189
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-17 00:07:44 +1300

    feat: Wrote functionality for createSaveGame

--- controllers/saveGame.js
+++ controllers/saveGame.js
@@ -22,21 +22,49 @@ const createSaveGame = async (req, res) => {
     saveGame = await primsa.saveGame.create({
       "data": {
         "id": req.body.id,
-        "money": req.body.store.money,
+        "money": req.body.money,
+      },
+    });
+
+    // Create save for placed items
+    const store = await primsa.store.create({
+      "data": {
+        "id": req.body.id,
+      },
+    });
+
+    const objectsCopy = req.body.store.store_objects;
+    objectsCopy.map((so, i) => req.body.store.store_objects[i].store_id = store.id);
+
+    await primsa.storeObject.createMany({
+      "data": req.body.store.store_objects,
+    });
+
+    // Create save for inventory
+    const inventory = await primsa.inventory.create({
+      "data": {
+        "id": req.body.id,
+      },
+    });
+
+    const itemsCopy = req.body.inventory.items;
+    itemsCopy.map((item, i) => req.body.inventory.items[i].inventory_id = inventory.id);
+
+    await primsa.inventoryItem.createMany({
+      "data": req.body.inventory.items,
+    });
+
+    saveGame = await primsa.saveGame.findUnique({
+      "where": { "id": String(req.body.id) },
+      "include": {
         "store": {
-          "create": {
-            "id": req.body.id,
-            "store_objects": {
-              "create": req.body.store.store_objects,
-            },
+          "include": {
+            "store_objects": true
           },
         },
         "inventory": {
-          "create": {
-            "id": req.body.id,
-            "items": {
-              "create": req.body.inventory.items,
-            },
+          "include": {
+            "items": true
           },
         },
       },
