d74d419aa945967d4d49e20121be598d0c79f42f
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-17 01:13:42 +1300

    docs: Added description to /save_games POST route

--- routes/api/saveGames/index.js
+++ routes/api/saveGames/index.js
@@ -4,6 +4,7 @@ import { validateCreateSaveGame } from "../../../middleware/validation.js";
 
 const router = express.Router();
 
+// Run createSaveGame on any POST request made to the /save_games route.
 router.post("/", validateCreateSaveGame, createSaveGame);
 
 export default router;
