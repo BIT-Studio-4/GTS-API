1bce4582dd689bf91382e9d62b5bd65423775bf8
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-17 00:59:20 +1300

    feat: Connected new validation to createSaveGame

--- routes/api/saveGames/index.js
+++ routes/api/saveGames/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createSaveGame } from "../../../controllers/saveGame.js";
+import { validateCreateSaveGame } from "../../../middleware/validation.js";
 
 const router = express.Router();
 
-router.post("/", createSaveGame);
+router.post("/", validateCreateSaveGame, createSaveGame);
 
 export default router;
