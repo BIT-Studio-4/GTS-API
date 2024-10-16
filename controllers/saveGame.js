d0e086690d30433ece17ce80e582285d50f50b66
wilsjw <wilsjw2@student.op.ac.nz>
2024-10-16 23:27:52 +1300

    feat: Created createSaveGame in saveGame controllers

--- controllers/saveGame.js
+++ controllers/saveGame.js
@@ -1,3 +1,54 @@
 import { PrismaClient } from "@prisma/client";
 
 const primsa = new PrismaClient();
+
+const createSaveGame = async (req, res) => {
+  try {
+    const contentType = req.headers["content-type"];
+    if (!contentType || contentType !== "application/json")
+      return res.status(400).json({
+        "msg": "Invalid Content-Type. Expected 'application/json'."  
+      });
+    
+    let saveGame = await primsa.saveGame.findUnique({
+      "where": { "id": String(req.body.id) },
+    });
+
+    if (saveGame) 
+      return res.status(403).json({
+        "msg": "Game save already exists!",
+      });
+    
+    saveGame = await primsa.saveGame.create({
+      "data": {
+        "id": req.body.id,
+        "money": req.body.store.money,
+        "store": {
+          "create": {
+            "id": req.body.id,
+            "store_objects": {
+              "create": req.body.store.store_objects,
+            },
+          },
+        },
+        "inventory": {
+          "create": {
+            "id": req.body.id,
+            "items": {
+              "create": req.body.inventory.items,
+            },
+          },
+        },
+      },
+    });
+
+    return res.status(201).json({
+      "msg": "Game save successfully created",
+      "data": saveGame,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      "msg": error.message,
+    });
+  }
+};
