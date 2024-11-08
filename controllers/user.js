bce3fa5d25c61747403483bdb1d4d06b2a405b4e
wilsjw <wilsjw2@student.op.ac.nz>
2024-11-08 15:52:23 +1300

    fix: Fixed getUsers according to changes requested

--- controllers/user.js
+++ controllers/user.js
@@ -42,16 +42,12 @@ const createUser = async (req, res) => {
 
 const getUsers = async (req, res) => {
   try {
-    const users = await prisma.user.findMany({
-      // Request only the name and money fields when querying the database. Makes sure it won't provide anything confidential or dangerous.
-      "select": {
-        "id": true,
-        "name": true,
-      },
-    });
+    const users = await prisma.user.findMany();
 
     // If the database couldn't find any users, return a 404 not found response.
     if (users.length === 0) return res.status(404).json({ "msg": "No users found." });
+
+    users.forEach(user => delete user.password);
 
     // If the query was successful, return the users with a 200 success response.
     return res.status(200).json({ 
